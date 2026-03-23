b1b253bc3ca24dd4e500a3f540f28f32900d11db
Panagiotis Giannou <panagiotisgia@outlook.com>
2026-03-23 15:47:08 +0200

    Add files via upload

---
+++ ThePropertiesOfArrows/ArrowTypes.cs
@@ -0,0 +1,16 @@
+/*
+* We create two enumarations for the arrowhead and flecthing to iterate the materials quickly
+*/
+enum ArrowHead
+{
+    Steel,
+    Wood,
+    Obsidiant
+}
+
+enum Fletching
+{
+    Plastic,
+    Turkey,
+    Goose
+}
